6a4abfd36a76e938d3f25a19bfa587337495cd97
Brianna Greenlaw <brianna.greenlaw@staples.com>
2018-12-13 13:13:13 -0500

    Greenlaw-code chalenge for Fidelity Senior UI Position

--- greenlaw_SeniorUI_CodeChallenge/README.md
+++ greenlaw_SeniorUI_CodeChallenge/README.md
@@ -9,7 +9,7 @@ Included Files:
 *factorial.js
 
 Notes about the files:
-	* For testing, each file includes the unit tests created as part of my test-driven development. They are located at the end of the files, and there
+	* For testing, each file includes the test cases created as part of my test-driven development. They are located at the end of the files, and there
 	is a JSFiddle link to accommodate viewing the testing on different machines (that might have different testing configurations.)
 	* The problems were chosen in combination with difficulty and not already having a JS solution inside the forked repository.
 	* The original BST Height README has been updated to reflect the correct height parameters, the problem used to have an example that if there was 
--- greenlaw_SeniorUI_CodeChallenge/arrayRotate.js
+++ greenlaw_SeniorUI_CodeChallenge/arrayRotate.js
@@ -9,18 +9,21 @@
 function arrayRotate(arrayToRotate, stepsToRotateBy){
 	var counter;
   var tempElement;
- if(arrayToRotate.length > 0 && stepsToRotateBy > 0){
+  if(!Array.isArray(arrayToRotate)){
+  	return "Must supply an array.";
+  }
+  if(arrayToRotate.length <= 0){
+  	return "Must supply an array with at least one element.";
+  }
+ if(stepsToRotateBy <= 0){
+ 	return "Steps to rotate must be greater than 0."
+ }
   for(counter = 1; counter <= stepsToRotateBy; counter++){
   	tempElement = arrayToRotate.pop();
-    console.log(tempElement);
     arrayToRotate.unshift(tempElement);
-    console.log(arrayToRotate);
   }
-  console.log(arrayToRotate);
   return arrayToRotate;
-} else { 
-  return "Must supply an array with at least 1 element and steps to rotate by must be greater than 0.";
-  }
+ 
 }
 /*
  * Test Cases
@@ -33,12 +36,14 @@ document.querySelector("#test1").innerHTML = "Test 1 Input: Array: [1,2,3,4,5] S
 //Test 2: result should be [4,5,6,7,8,9,10,1,2,3]
 document.querySelector("#test2").innerHTML = "Test 2 Input: Array: [1,2,3,4,5,6,7,8,9,10] Steps:, Output: " + arrayRotate([1,2,3,4,5,6,7,8,9,10], 7) + " Test 2: result should be [4,5,6,7,8,9,10,1,2,3]";
 //Test 3: result should be  
-document.querySelector("#test3").innerHTML = "Test 3 Input: Array: [1,2,3,4,5] Steps:0, Output: " + arrayRotate([1,2,3,4,5], 0) + " Test 3: result should be: Must supply an array with at least 1 element and steps to rotate by must be greater than 0.  ";
+document.querySelector("#test3").innerHTML = "Test 3 Input: Array: [1,2,3,4,5] Steps:0, Output: " + arrayRotate([1,2,3,4,5], 0) + " Test 3: Steps to rotate by must be greater than 0.  ";
 //Test 4: result should be 
-document.querySelector("#test4").innerHTML = "Test 4 Input: Array: [1,2,3,4,5] Steps: -1, Output: " + arrayRotate([1,2,3,4,5], -1) + " Test 4: result should be: Must supply an array with at least 1 element and steps to rotate by must be greater than 0.  ";
+document.querySelector("#test4").innerHTML = "Test 4 Input: Array: [1,2,3,4,5] Steps: -1, Output: " + arrayRotate([1,2,3,4,5], -1) + " Test 4: Steps to rotate by must be greater than 0.  ";
 //Test 5: result should be 
-document.querySelector("#test5").innerHTML = "Test 5 Input: Array: [] Steps: 3, Output: " + arrayRotate([], 3) + " Test 5: result should be: Must supply an array with at least 1 element and steps to rotate by must be greater than 0. ";
-//Test 6: result should be [1,2,3,4,5]
-document.querySelector("#test6").innerHTML = "Test 6 Input: Array: [1,2,3,4,5] Steps: 5, Output: " + arrayRotate([1,2,3,4,5], 5) + " Test 6: result should be [1,2,3,4,5]";
+document.querySelector("#test5").innerHTML = "Test 5 Input: Array: [] Steps: 3, Output: " + arrayRotate([], 3) + " Test 5: result should be: Must supply an array with at least one element. ";
+//Test 6: result should be 
+document.querySelector("#test6").innerHTML = "Test 6 Input: Array: 'hello world' Steps: 5, Output: " + arrayRotate("hello world", 5) + " Test 6: Must supply an array.";
+//Test 7: result should be 
+document.querySelector("#test7").innerHTML = "Test 7 Input: Array: null Steps: 5, Output: " + arrayRotate(null, 5) + " Test 7: Must supply an array.";
 
 
